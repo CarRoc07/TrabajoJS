75b21f6d229472ae654014903ceedd4f6667751d
CarRoc07 <carlovhd@gmail.com>
2022-09-12 20:32:59 -0300

    S3

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+//hamburguer menú consigna 1
 const iconoMenu = document.querySelector("#iconoMenu");
 const menu = document.querySelector(".content-nav");
-
+//ToDo Consigna 3
+const inputToDo = document.querySelector(".input-toDo");
+const btnSend = document.querySelector(".btn-send");
+const tasksContainer = document.querySelector(".content-toDo");
 
 iconoMenu.addEventListener("click", (e) =>{
     menu.classList.toggle("active");
